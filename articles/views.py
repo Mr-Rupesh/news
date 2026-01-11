f2c2afd8f9ff0ddb48f5dc5436c0ba51526f17bf
Rupesh M <rupeshmalhipparge@gmail.com>
2026-01-12 00:51:59 +0530

    comments app

--- articles/views.py
+++ articles/views.py
@@ -1,20 +1,61 @@
-from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
-from django.views.generic import ListView, DetailView
+from django.views import View
+from django.views.generic import ListView, DetailView, FormView  # new
+from django.views.generic.detail import SingleObjectMixin  # new
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy, reverse  # new
 
+from .forms import CommentForm
 from .models import Article
 
-class ArticleListView(LoginRequiredMixin, ListView):
+
+class ArticleListView(LoginRequiredMixin, ListView):  # new
     model = Article
     template_name = "article_list.html"
 
-class ArticleDetailView(LoginRequiredMixin, DetailView):
+
+class CommentGet(DetailView):  # new
     model = Article
     template_name = "article_detail.html"
 
-class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+
+
+class CommentPost(SingleObjectMixin, FormView):  # new
+    model = Article
+    form_class = CommentForm
+    template_name = "article_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object
+        comment.author = self.request.user
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        article = self.object
+        return reverse("article_detail", kwargs={"pk": article.pk})
+
+
+class ArticleDetailView(LoginRequiredMixin, View):  # new
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
+
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):  # new
     model = Article
     fields = (
         "title",
@@ -22,18 +63,20 @@ class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     )
     template_name = "article_edit.html"
 
-    def test_func(self):
+    def test_func(self):  # new
         obj = self.get_object()
         return obj.author == self.request.user
 
-class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):  # new
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
 
-    def test_func(self):
+    def test_func(self):  # new
         obj = self.get_object()
         return obj.author == self.request.user
+
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
